c740c34fb40f086a6d86d6b82af06b15e28b2bdf
Kashyap Bhatt <kashyap.bhatt15@gmail.com>
2013-06-19 12:21:40 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CatSubscribe::Application.config.secret_token = 'c974adc92e869c43e5feaab7bc171753814483de58ac67fce1754e98747419d181c58043521359ab90536fab6feb39993120e9df66e86f9bd0b4a548c013c565'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+CatSubscribe::Application.config.session_store :cookie_store, key: '_catSubscribe_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# CatSubscribe::Application.config.session_store :active_record_store
